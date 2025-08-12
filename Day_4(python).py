bb1b9fd82ed96633bd546e1f3873f559f153b46b
annareddy-rakeshreddy <rakeshreddyannareddy7@gmail.com>
2025-08-12 09:57:23 +0530

    Add files via upload

---
+++ Day_4(python).py
@@ -0,0 +1,73 @@
+# # a=int(input("no"))
+# # b=int(input("no"))
+# # o=input("op")
+
+# # def add(a,b):
+# #     print(a+b)
+
+# # def sub(a,b):
+# #     print(a-b)
+
+# # def mul(a,b): 
+# #     print(a*b)
+# # def div(a,b):
+# #     print(a/b)
+
+# # if o=="+":
+# #     add(a,b)
+# # elif o=="-":
+# #     sub(a,b)
+# # elif o=="*":
+# #     mul(a,b)
+
+# # else:
+# #     div(a,b)
+
+# def  is_prime(m):
+#     is_prime=True
+#     if m<=1:
+#         is_prime=False
+#     else:
+
+
+# n = int(input())
+# for i in range(1, n):
+#     print(" " * (n - i) + "" + " " * (2 * i - 3) + ("" if i != 1 else ""))
+# else:
+#     print("*" * (2 * n - 1))
+   
+
+
+
+# n=str(input())
+# s=0
+# power =len(n)
+# for i in n:
+#     s+=int(i)**power
+# if s==int(n):
+#     print("is armstrong")
+# else:
+#     print("not armstrong")    
+
+
+
+
+# #prime or not in given num
+# def is_prime(m):
+#     is_prime=True
+#     if m <=1:
+#         is_prime=False
+#     else:
+#         for i in range(2,int(m**0.5)+1):
+#             if m%i==0:
+#                 is_prime=False         
+#                 break
+#     if is_prime:
+#         print(m,"is a prime")
+
+
+
+
+
+
+
